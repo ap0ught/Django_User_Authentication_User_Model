e104bcf82791a14819e913d00a504e0d41a1cea9
Christopher Mayfield <beta@ap0.org>
2024-09-10 00:16:40 -0500

    Refactor test setup methods in ProfileTestCase
    Extracted repetitive user and image creation logic into separate helper methods for better readability and maintainability. Adjusted references to the newly created profile picture path in the test cases to ensure consistency.

--- tests/test_models.py
+++ tests/test_models.py
@@ -9,33 +9,38 @@ from app.models import Profile
 
 
 class ProfileTestCase(TestCase):
+
     def setUp(self):
-        self.user = User.objects.create_user(username='testuser', password='12345')
+        self._create_test_user()
+        self._create_test_image()
         self.client = Client()
-        self.image_path = "./my_profile_pic.png"
-        self.image = Image.new('RGBA', size=(50, 50), color=(256, 0, 0))
-        self.image.save(self.image_path)
 
     def tearDown(self):
-        os.remove(self.image_path)
+        os.remove(self.profile_pic_path)
+
+    def _create_test_user(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+
+    def _create_test_image(self):
+        self.profile_pic_path = "./my_profile_pic.png"
+        self.profile_pic = Image.new('RGBA', size=(50, 50), color=(256, 0, 0))
+        self.profile_pic.save(self.profile_pic_path)
 
     def test_profile_creation(self):
-        Profile.objects.create(user=self.user, address="test address", profile_pic=self.image_path)
+        Profile.objects.create(user=self.user, address="test address", profile_pic=self.profile_pic_path)
         self.assertEqual(Profile.objects.count(), 1)
         self.assertEqual(Profile.objects.get(user=self.user).address, "test address")
 
     def test_profile_pic(self):
-        with open(self.image_path, 'rb') as img:
+        with open(self.profile_pic_path, 'rb') as img:
             file_obj = SimpleUploadedFile('my_profile_pic.png', img.read(), content_type='image/png')
             profile = Profile.objects.create(user=self.user, address="test address", profile_pic=file_obj)
             profile.save()
-
         self.assertIsNotNone(profile.profile_pic.path)
         self.assertTrue(os.path.exists(profile.profile_pic.path))
 
     def test_profile_deletion(self):
-        profile = Profile.objects.create(user=self.user, address="test address", profile_pic=self.image_path)
+        profile = Profile.objects.create(user=self.user, address="test address", profile_pic=self.profile_pic_path)
         user_id = profile.user.id
         profile.user.delete()
-
         self.assertFalse(Profile.objects.filter(user_id=user_id).exists())
